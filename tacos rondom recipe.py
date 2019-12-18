c5a6cfdb52b4b487600ea99981304a8c8a423b01
kingc <fk3737nf@go.minneapolis.edu>
2019-12-18 17:03:10 -0600

    taco recipe codes

--- tacos rondom recipe.py
+++ tacos rondom recipe.py
@@ -13,9 +13,31 @@ image.thumbnail((800, 800))                  # resizing image to 800 using image
 
 # the next step is going to be image .show()  so that it shows my image when i run it
 
+
+
+# next step i'm going  to draw a text around my image
+
+draw = ImageDraw.Draw(image)
+
+# for next one i'm going to use font color for text on the image. so i downloaded DjaVusans from the website
+
+font = ImageFont.truetype('DejaVuSans (1).ttf', 40)    # this will create the font text on the image and choose 40 for the font size
+
+
+# 40 as for font size
+
+# for the draw text around my image i'm using draw.text to draw text on my image
+draw.text([10,475], 'Random Taco Cookbook', fill= 'red', font = font)   # for that is going to be Random Taco Cookbook
+# use 10, 475 to draw the text and the text will show on the image.
+# for the color i choose red.
+
+
 image.show()  # this shows the image when every thing is typed and resized the picture it will show the outcome
 
 #  since i finished everything i'm going to go head and save my picture so that it appears on the project side
 
 image.save('Rondom tacos recipe .jpeg')   # Saving the image as this will show up on the project side and i will be able to access it
+
+
+
 
